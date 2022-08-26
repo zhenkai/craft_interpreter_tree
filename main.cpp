d7595df22b5f7889b1eb99542e50912932f593b5
Zhenkai Zhu <zhenkai@totoro.local>
2022-08-26 10:12:53 -0700

    main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <cstdlib>
+
+namespace {
+  void run(const std::string& source) {
+    std::cout << source << std::endl;
+  }
+  
+  void runFile(const std::string& path) {
+    std::ifstream t(path);
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    run(buffer.str());
+  }
+
+  void runPrompt() {
+    while(true) {
+      std::cout << "> ";
+      std::string line;
+      std::getline(std::cin, line);
+      run(line);
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   switch(argc) {
-    case 0:
-      // runPrompt(); break;
     case 1:
-      // runFile(argv[0]); break;
+      runPrompt(); break;
+    case 2:
+      runFile(argv[0]); break;
     default:
       std::cout << "Usage: lox [script]" << std::endl;
       return 64;
